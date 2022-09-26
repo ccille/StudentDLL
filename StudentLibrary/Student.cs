839ed4717cfb1b2cdae557bc19f228ad8942e2a6
Cecilie Maria Hernig <ceci7542@edu.easj.dk>
2022-09-26 16:03:18 +0200

    Add project files.

---
+++ StudentLibrary/Student.cs
@@ -0,0 +1,28 @@
+﻿namespace StudentLibrary
+{
+    public class Student
+    {
+        public string Name { get; set; }
+
+        public string Adress { get; set; }
+
+        public Student(string name, string adress)
+        {
+            Name = name;
+            Adress = adress;
+        }
+
+        public Student()
+        {
+        }
+
+        /// <summary>
+        /// Metode til navn og adresse i en tekst streng
+        /// </summary>
+        /// <returns>navn og adress på en studerende</returns>
+        public string NameAdresstext()
+        {
+            return "Name: " + Name + " Adress: " + Adress;
+        }
+    }
+}
---
+++ StudentLibraryTests/StudentTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StudentLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentLibrary.Tests
+{
+    [TestClass()]
+    public class StudentTests
+    {
+        [TestMethod()]
+        [DataRow("cv")]
+        public void StudentTest(string value)
+        {
+            //Arrange
+            Student stu = new Student();
+
+            //Act
+            string expected = "cv";
+
+            //Assert
+            Assert.AreEqual(expected, value);
+        }
+
+
+    }
+}
